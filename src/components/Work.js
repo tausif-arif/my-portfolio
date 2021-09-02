47fd8d27b4aa12f248b8874e8a712ab3d5f4a7b5
Tausif Arif <tausifarif2gmail.com>
2021-09-02 15:18:42 +0530

    solve link problem

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,8 +14,8 @@ const Work = () => {
     <div className="main_work">
       <h1 style={{textAlign:"center",marginTop:"20px",textTransform:" uppercase",fontSize:"1rem"}}>check out some of my work</h1>
       <div className="workContainer">
-      <WorkCard title="Weather 1.0" desc="a weather app" img={weatherimg} url={link.clicks}/>
-      <WorkCard title="Clicks" desc="an unsplash clone" img={clicksimg} url={link.weather}/>
+      <WorkCard title="Weather 1.0" desc="a weather app" img={weatherimg} url={link.weather}/>
+      <WorkCard title="Clicks" desc="an unsplash clone" img={clicksimg} url={link.clicks}/>
       
       </div>
     </div>
