36f811d903a9498d46f7ab8eacacf168d94fee9f
Tausif Arif <tausifarif2gmail.com>
2021-09-02 16:24:11 +0530

    fixing bugs

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,12 @@ const About = () => {
       </div>
       <div className="inner_abt">
         <p className="abt_text">
-          Hi! I am Tausif.I am a fresher in web development. My Education
+          Hi! I am Tausif Arif.I am a fresher in web development. My Education
           qualification is BCA(Bachelor of Computer Application). I have diverse
           set of skills ranging from design to HTML + CSS + JAVASCRIPT, all the
-          way to REACTJS.
+          way to REACTJS and also idea about oracle.I am ready to handle
+          multiple task on a daily basis and always eager to learn new skill so
+          that i give my best to my organization.
         </p>
       </div>
     </div>
